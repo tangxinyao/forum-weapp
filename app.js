32f2a06b2404a909db18c4f0d2f07e148ada9e78
tang <2578873659@qq.com>
2017-11-22 01:16:04 +0800

    promisify api, but there is still a bug to be fixed when trying convert code to es5.

--- app.js
+++ app.js
@@ -1,46 +1,87 @@
 //app.js
 App({
-  onLaunch: function () {
+  onLaunch: async function () {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        if (res.code) {
-          wx.request({
-            url: `http://${this.globalData.hostname}:${this.globalData.port}/sessions`,
-            data: {
-              code: res.code
-            }
-          })
-        } else {
-          console.log(res.errMsg)
-        }
+    try {
+      const setting = await this.requestSetting();
+      if (!setting.authSetting['scope.userInfo']) {
+        await requestAuth('scope.userInfo');
       }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
+      const code = await this.requestCode();
+      const userInfo = await this.requestUserInfo();
+      if (code) {
+        await this.transportUserInfo(code, userInfo.encryptedData, userInfo.iv, userInfo.userInfo);
       }
-    })
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  requestSetting: function () {
+    return new Promise((resolve, reject) => {
+      wx.getSetting({
+        success: (res) => {
+          resolve(res);
+        },
+        fail: (res) => {
+          reject(res);
+        }
+      })
+    });
+  },
+  requestAuth: function (scope) {
+    return new Promise((resolve, reject) => {
+      wx.authorize({
+        scope,
+        success: (res) => {
+          resolve(res);
+        },
+        fail: (res) => {
+          reject(res);
+        }
+      })
+    });
+  },
+  requestCode: function () {
+    return new Promise((resolve, reject) => {
+      wx.login({
+        success: (res) => {
+          resolve(res.code);
+        },
+        fail: (res) => {
+          reject(res);
+        }
+      });
+    });
+  },
+  requestUserInfo: function () {
+    return new Promise((resolve, reject) => {
+      wx.getUserInfo({
+        success: res => {
+          resolve(res);
+        },
+        fail: res => {
+          reject(res);
+        }
+      });
+    });
+  },
+  transportUserInfo: function (code, encryptedData, iv, userInfo) {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url: `http://${this.globalData.hostname}:${this.globalData.port}/sessions`,
+        data: { code, encryptedData, iv, userInfo },
+        success: (res) => {
+          resolve(res);
+        },
+        fail: (res) => {
+          reject(res);
+        }
+      });
+    });
   },
   globalData: {
     hostname: 'localhost',
