a72c8e09c17232c7154595318654d5bc5d8d8292
tang <2578873659@qq.com>
2017-11-23 00:28:20 +0800

    use callback other than async/await

--- app.js
+++ app.js
@@ -1,87 +1,85 @@
 //app.js
 App({
-  onLaunch: async function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    try {
-      const setting = await this.requestSetting();
-      if (!setting.authSetting['scope.userInfo']) {
-        await requestAuth('scope.userInfo');
+  onLaunch: function () {
+    this.requestAuth(() => {
+      this.requestLoginInfo((codeInfo, userInfo) => {
+        this.transportLoginInfo(codeInfo, userInfo, (token) => {
+          console.log(token);
+        }, this.handleError);
+      }, this.handleError);
+    }, this.handleError);
+  },
+  /**
+   * @param {callback} next - no arg
+   * @param {callback} err - one arg
+   */
+  requestAuth: function (next, err) {
+    wx.getSetting({
+      success: (setting) => {
+        if (!setting.authSetting['scope.userInfo']) {
+          wx.authorize({
+            scope: 'scope.userInfo',
+            success: () => {
+              next();
+            },
+            fail: (errRes) => {
+              err(errRes);
+            }
+          });
+        } else {
+          next();
+        }
+      },
+      fail: (res) => {
+        err(res);
       }
-      const code = await this.requestCode();
-      const userInfo = await this.requestUserInfo();
-      if (code) {
-        await this.transportUserInfo(code, userInfo.encryptedData, userInfo.iv, userInfo.userInfo);
+    });
+  },
+  /**
+   * @param {callback} next - two args, codeInfo & userInfo
+   * @param {callback} err - one arg
+   */
+  requestLoginInfo: function (next, err) {
+    wx.login({
+      success: (codeInfo) => {
+        wx.getUserInfo({
+          success: (userInfo) => {
+            next(codeInfo, userInfo);
+          },
+          fail: (errRes) => {
+            err(errRes);
+          }
+        });
+      },
+      fail: (errRes) => {
+        err(errRes);
       }
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  requestSetting: function () {
-    return new Promise((resolve, reject) => {
-      wx.getSetting({
-        success: (res) => {
-          resolve(res);
-        },
-        fail: (res) => {
-          reject(res);
-        }
-      })
     });
   },
-  requestAuth: function (scope) {
-    return new Promise((resolve, reject) => {
-      wx.authorize({
-        scope,
-        success: (res) => {
-          resolve(res);
-        },
-        fail: (res) => {
-          reject(res);
-        }
-      })
+  /**
+   * @param {Obejct} codeInfo
+   * @param {Object} userInfo
+   * @param {callback} next - one arg, token
+   * @param {callback} err - one arg
+   */
+  transportLoginInfo: function (codeInfo, userInfo, next, err) {
+    wx.request({
+      url: `http://${this.globalData.hostname}:${this.globalData.port}/sessions`,
+      data: {
+        code: codeInfo.code,
+        encryptedData: userInfo.encryptedData,
+        iv: userInfo.iv
+      },
+      success: (response) => {
+        next(response.data);
+      },
+      fail: (errRes) => {
+        err(errRes);
+      }
     });
   },
-  requestCode: function () {
-    return new Promise((resolve, reject) => {
-      wx.login({
-        success: (res) => {
-          resolve(res.code);
-        },
-        fail: (res) => {
-          reject(res);
-        }
-      });
-    });
-  },
-  requestUserInfo: function () {
-    return new Promise((resolve, reject) => {
-      wx.getUserInfo({
-        success: res => {
-          resolve(res);
-        },
-        fail: res => {
-          reject(res);
-        }
-      });
-    });
-  },
-  transportUserInfo: function (code, encryptedData, iv, userInfo) {
-    return new Promise((resolve, reject) => {
-      wx.request({
-        url: `http://${this.globalData.hostname}:${this.globalData.port}/sessions`,
-        data: { code, encryptedData, iv, userInfo },
-        success: (res) => {
-          resolve(res);
-        },
-        fail: (res) => {
-          reject(res);
-        }
-      });
-    });
+  handleError: function (err) {
+    console.log(err);
   },
   globalData: {
     hostname: 'localhost',
